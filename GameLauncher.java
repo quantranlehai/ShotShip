18998e4af2f2fd56e11aded2a4a3e666002b5ce9
Quan Tran <tranlehaiquan1996@gmail.com>
2016-02-27 16:36:09 +0700

    JavaHeartFirst

---
+++ GameLauncher.java
@@ -0,0 +1,6 @@
+public class GameLauncher{
+	public static void main(String[] agrs){
+		GuessGame game = new GuessGame();
+		game.StartGame();
+	}
+}
---
+++ remember.java
@@ -0,0 +1,6 @@
+public class remember{
+	public static void main (String[] args){
+	System.out.println("Next");
+	System.out.println("Again");
+	}
+}
